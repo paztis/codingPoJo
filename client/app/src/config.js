9644fc608233152bf2eb16e7a2290756e83f1b68
P N <pnoc@users.noreply.github.com>
2016-03-08 13:57:41 +0100

    creation du config.js

---
+++ client/app/src/config.js
@@ -0,0 +1,6 @@
+require.config({
+    paths: {
+        'jquery': '../lib/jquery/dist/jquery',
+        'underscore': '../lib/underscore-amd/underscore'
+    }
+});
--- client/app/src/main.js
+++ client/app/src/main.js
@@ -1,9 +1,4 @@
-require.config({
-    paths: {
-        'jquery': '../lib/jquery/dist/jquery',
-        'underscore': '../lib/underscore-amd/underscore'
-    }
-});
+
 
 require(['jquery'], function ($) {
 
